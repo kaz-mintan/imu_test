1134eb337791b2f4cb04ecb72e4f80b18f8dea34
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-11-21 09:42:36 +0900

    add some function but it doesn't work now

--- mpu9250/main.cpp
+++ mpu9250/main.cpp
@@ -6,4 +6,13 @@ int main(){
 	printf("hello\n");
 	mog_mpu.initialize();
 	mog_mpu.testConnection();
+
+	float ax,ay,az;
+	float gx,gy,gz;
+
+	//void MPU9250::getMotion9(float *ax, float *ay, float *az, float *gx, float *gy, float *gz, float *mx, float *my, float *mz)
+	while(1){
+		mog_mpu.getMotion6(&ax,&ay,&az,&gx,&gy,&gz);
+		printf("hoge,%f%f%f\n",ax,ay,az);
+	}
 }
